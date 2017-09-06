e12a8ce1d9b0ad3b35cbf136dd386ddeed51ec84
Stan Seibert <stan.seibert@continuum.io>
2017-09-06 18:26:32 -0500

    Do not import half-finished hdf5 plugins right now

--- intake/plugins/__init__.py
+++ intake/plugins/__init__.py
@@ -3,26 +3,8 @@ import traceback
 
 registry = {}
 
-try:
-    from . import csv
-    registry['csv'] = csv.Plugin()
-except Exception as e:
-    traceback.print_exc()
-    print('Could not add csv plugin')
-
-try:
-    from . import hdf5_array
-    registry['hdf5_array'] = hdf5_array.Plugin()
-except Exception as e:
-    traceback.print_exc()
-    print('Could not add hdf5_array plugin')
-
-try:
-    from . import hdf5_dataframe
-    registry['hdf5_dataframe'] = hdf5_dataframe.Plugin()
-except Exception as e:
-    traceback.print_exc()
-    print('Could not add hdf5_dataframe plugin')
+from . import csv
+registry['csv'] = csv.Plugin()
 
 try:
     from . import postgres
