f4b189e39af829f541b19aa5a6373dc004632048
Stan Seibert <stan.seibert@continuum.io>
2017-10-04 17:19:41 -0500

    remove half-baked postgres driver

--- intake/source/__init__.py
+++ intake/source/__init__.py
@@ -5,10 +5,3 @@ registry = {}
 
 from . import csv
 registry['csv'] = csv.Plugin()
-
-try:
-    from . import postgres
-    registry['postgres'] = postgres.Plugin()
-except Exception as e:
-    traceback.print_exc()
-    print('Could not add hdf5 plugin')
