73be37e2f86ffcc5f9a91cd4e81658c8550d1427
Martin Durant <martin.durant@utoronto.ca>
2018-02-28 10:15:37 -0500

    Conda prefix fallback finding

--- intake/catalog/default.py
+++ intake/catalog/default.py
@@ -1,7 +1,10 @@
 import appdirs
+import json
 import os
+import subprocess
 
 from .base import Catalog
+
 
 def make_empty_catalog():
     # TODO: replace with Catalog() (or None or []) and implement in the constructor
@@ -10,7 +13,7 @@ def make_empty_catalog():
 
 
 def load_user_catalog():
-    '''Return a catalog for the platform-specific user Intake directory'''
+    """Return a catalog for the platform-specific user Intake directory"""
     cat_dir = user_data_dir()
     if not os.path.isdir(cat_dir):
         return make_empty_catalog()
@@ -19,12 +22,12 @@ def load_user_catalog():
 
 
 def user_data_dir():
-    '''Return the user Intake catalog directory'''
+    """Return the user Intake catalog directory"""
     return appdirs.user_data_dir(appname='intake', appauthor='intake')
 
 
 def load_global_catalog():
-    '''Return a catalog for the environment-specific Intake directory'''
+    """Return a catalog for the environment-specific Intake directory"""
     cat_dir = global_data_dir()
     if not os.path.isdir(cat_dir):
         return make_empty_catalog()
@@ -36,23 +39,34 @@ CONDA_VAR = 'CONDA_PREFIX'
 VIRTUALENV_VAR = 'VIRTUAL_ENV'
 
 
+def conda_prefix():
+    """Fallback: ask conda in PATH for its prefix"""
+    try:
+        out = subprocess.check_output(['conda', 'info', '--json'])
+        return json.loads(out.decode())["conda_prefix"]
+    except (subprocess.CalledProcessError, json.JSONDecodeError):
+        return False
+
+
 def global_data_dir():
-    '''Return the global Intake catalog dir for the current environment'''
+    """Return the global Intake catalog dir for the current environment"""
 
     if CONDA_VAR in os.environ:
         prefix = os.environ[CONDA_VAR]
     elif VIRTUALENV_VAR in os.environ:
         prefix = os.environ[VIRTUALENV_VAR]
     else:
-        # Return immediately since appdirs constructs full path
+        prefix = conda_prefix()
+    
+    if prefix:
+        # conda and virtualenv use Linux-style directory pattern
+        return os.path.join(prefix, 'share', 'intake')
+    else:
         return appdirs.site_data_dir(appname='intake', appauthor='intake')
-
-    # conda and virtualenv use Linux-style directory pattern
-    return os.path.join(prefix, 'share', 'intake')
 
 
 def load_combo_catalog():
-    '''Load a union of the user and global catalogs for convenience'''
+    """Load a union of the user and global catalogs for convenience"""
     user_dir = user_data_dir()
     global_dir = global_data_dir()
 
@@ -62,7 +76,8 @@ def load_combo_catalog():
     if os.path.isdir(global_dir):
         cat_dirs.append(global_dir)
 
-    # TODO: if we find no dirs or dirs are empty, Catalog should cope and return empty without this branching
+    # TODO: if we find no dirs or dirs are empty,
+    # Catalog should cope and return empty without this branching
     if len(cat_dirs) > 0:
         return Catalog(cat_dirs)
     else:
