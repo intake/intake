2e1fc2a8173aed522e82e9722715431e01a24fce
Martin Durant <martin.durant@utoronto.ca>
2018-04-08 09:31:24 -0400

    typo

--- intake/catalog/default.py
+++ intake/catalog/default.py
@@ -44,7 +44,7 @@ def conda_prefix():
     try:
         out = subprocess.check_output(['conda', 'info', '--json'])
         return json.loads(out.decode())["default_prefix"]
-    except (subprocess.CalledProcessError, json.JSONDecodeErrorm, OSError):
+    except (subprocess.CalledProcessError, json.JSONDecodeError, OSError):
         return False
 
 
