1e41db012d5a5e7ec055c3953ea31c810a61938c
Martin Durant <martin.durant@utoronto.ca>
2018-08-13 09:37:16 -0400

    Add ipywidget GUI behaviour

--- intake/gui/widgets.py
+++ intake/gui/widgets.py
@@ -32,16 +32,25 @@ class DataBrowser(object):
         self.update_cat_list()
         self.fs = None
         self.item_list = widgets.Select()
-        self.add = widgets.Button(icon='plus-square',
+        self.detail = widgets.Textarea(disabled=True,
+                                       placeholder='Item Description')
+        self.add = widgets.Button(
+            icon='plus-square',
+            tooltip='Add Catalog',
             layout=widgets.Layout(flex='1 1 auto', width='auto'))
         self.add.on_click(self.add_cat)
-        self.url = widgets.Text(value='Catalog URL',
+        self.lurl = widgets.Label(value='URL:')
+        self.url = widgets.Text(value=os.getcwd(),
             layout=widgets.Layout(flex='10 1 auto', width='auto'))
-        self.files = widgets.Button(icon='folder-open',
+        self.files = widgets.Button(
+            icon='folder-open',
+            tooltip='Open File Selector',
             layout=widgets.Layout(flex='1 1 auto', width='auto'))
         self.files.on_click(self.openfs)
-        self.mid = widgets.HBox(children=[self.cat_list, self.item_list])
-        self.bottom = widgets.HBox(children=[self.url, self.files, self.add])
+        self.mid = widgets.HBox(children=[self.cat_list,
+                                          self.item_list, self.detail])
+        self.bottom = widgets.HBox(children=[self.lurl, self.url, self.files,
+                                             self.add])
         self.widget = widgets.VBox(children=[self.mid, self.bottom])
         self.cat_selected({'new': list(self.cats)[0]})
         self.cat_list.observe(self.cat_selected, 'value')
@@ -58,17 +67,19 @@ class DataBrowser(object):
         names = [n + '  ->' if self.cats[name][n].container == 'catalog'
                  else n for n in self.cats[name]]
         self.item_list.options = names
+        self.item_selected({'new': names[0]}, first=True)
 
-    def item_selected(self, ev):
+    def item_selected(self, ev, first=False):
         name = ev['new']
         if name is None:
             return
-        if name.endswith('  ->'):
+        if name.endswith('  ->') and not first:
             cat = self.cats[self.cat_list.value][name[:-4]]
             self.cats[self.cat_list.value + '.' + cat.name] = cat()
             self.update_cat_list()
         else:
             self.item = self.cats[self.cat_list.value][name]
+            self.detail.value = str(self.item.describe())
 
     def add_cat(self, ev):
         fn = self.url.value
@@ -83,8 +94,9 @@ class DataBrowser(object):
         self.fs = FileSelector(self.file_chosen)
         self.widget.children = [self.mid, self.bottom, self.fs.selector]
 
-    def file_chosen(self, fn):
-        self.url.value = fn
+    def file_chosen(self, fn, ok=True):
+        if ok:
+            self.url.value = fn
         self.widget.children = [self.mid, self.bottom]
 
 
@@ -93,15 +105,22 @@ class FileSelector(object):
         self.done = done_callback
         self.path = os.getcwd() + '/'
         self.widget = widgets.Select(rows=15)
-        self.button = widgets.Button(icon='chevron-left',
+        self.button = widgets.Button(
+            icon='chevron-left', tooltip='Parent',
             layout=widgets.Layout(flex='1 1 auto', width='auto'))
         self.button.on_click(self.up)
         self.label = widgets.Label(
             layout=widgets.Layout(flex='100 1 auto', width='auto'))
-        self.button2 = widgets.Button(icon='check',
+        self.x = widgets.Button(
+            icon='close', tooltip='Close Selector',
+            layout=widgets.Layout(flex='1 1 auto', width='auto'))
+        self.x.on_click(self.stop)
+        self.button2 = widgets.Button(
+            icon='check', tooltip='OK',
             layout=widgets.Layout(flex='1 1 auto', width='auto'))
         self.button2.on_click(self.stop)
-        self.label2 = widgets.Label(value='<no file setected>',
+        self.label2 = widgets.Label(
+            value='<no file selected>',
             layout=widgets.Layout(flex='100 1 auto', width='auto'))
         self.make_options()
         self.widget.observe(self.changed, 'value')
@@ -109,7 +128,7 @@ class FileSelector(object):
             self.button, self.label
         ])
         self.lower = widgets.Box(children=[
-            self.button2, self.label2
+            self.x, self.button2, self.label2
         ])
         self.selector = widgets.VBox(children=[
             self.upper, self.widget, self.lower
@@ -145,8 +164,7 @@ class FileSelector(object):
         self.widget.unobserve_all()
         self.button.unobserve_all()
         self.button2.unobserve_all()
-        self.widget.disabled = True
-        self.button.disabled = True
-        self.button2.disabled = True
+        self.x.unobserve_all()
+        ok = ev.tooltip == 'OK' and self.label2.value != '<no file selected>'
         if self.done is not None:
-            self.done(self.label2.value)
+            self.done(self.label2.value, ok=ok)
