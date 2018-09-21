02bd5ca6d0d19a8e9662e4167928b8e531e54bbc
Martin Durant <martin.durant@utoronto.ca>
2018-09-21 19:25:37 -0400

    Start implementation

--- intake/source/__init__.py
+++ intake/source/__init__.py
@@ -1,2 +1,29 @@
+import logging
+logger = logging.getLogger('intake')
+
 # The registry is the mapping of plugin name (aka driver) to DataSource class
 registry = {}
+
+# A set of fully-qualifies package.module.Class mappings
+classes = {}
+
+
+def get_plugin_class(name):
+    if name in registry:
+        return registry[name]
+    if '.' not in name:
+        logger.warning('Plugin name "%s" not known' % name)
+        return None
+    if name not in classes:
+        import importlib
+        mod, cls = name.rsplit('.', 1)
+        try:
+            module = importlib.import_module(mod)
+        except ImportError:
+            logger.debug('Import of plugin module "%s" failed' % mod)
+            return None
+        try:
+            classes[name] = getattr(module, cls)
+        except (KeyError, NameError):
+            logger.debug('Class "%s" not found in module "%s"' % (cls, mod))
+    return classes.get(name, None)
