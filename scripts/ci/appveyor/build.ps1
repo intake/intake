4e711cca1c7044144af33c1f2ed42e5a4ab58dff
Julia Signell <jsignell@gmail.com>
2019-01-17 16:51:27 -0500

    Switching to bokeh approach

---
+++ scripts/ci/appveyor/build.ps1
@@ -0,0 +1,5 @@
+function build(){
+    conda install -y --use-local intake
+}
+
+build
---
+++ scripts/ci/appveyor/install.ps1
@@ -0,0 +1,11 @@
+function install() {
+    conda config --set auto_update_conda off
+    conda config --set always_yes yes
+    conda config --add channels conda-forge
+    conda config --get channels
+    conda update -q conda
+    conda install jinja2 pyyaml
+    conda install $(python scripts/deps.py)
+}
+
+install
