a1bb9a27f8a8e098fac6e04b31781e10ecb0768b
Dan Allan <dallan@bnl.gov>
2019-02-20 16:21:56 -0500

    Put back 'or {}'

--- intake/catalog/remote.py
+++ intake/catalog/remote.py
@@ -39,7 +39,7 @@ class RemoteCatalogEntry(CatalogEntry):
         self.container = container
         self.name = name
         self.description = description
-        self._metadata = metadata
+        self._metadata = metadata or {}
         self._page_size = page_size
         self._user_parameters = user_parameters or []
         self._direct_access = direct_access
