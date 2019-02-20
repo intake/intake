c6088caf3bce9518174d56421d359843e2c104d5
Martin Durant <martin.durant@utoronto.ca>
2019-02-20 16:37:38 -0500

    add sleeps for windows test

--- intake/container/tests/test_persist.py
+++ intake/container/tests/test_persist.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from intake.container.persist import store
 from intake.source.base import DataSource
@@ -8,13 +9,20 @@ def test_store(temp_cache):
     assert list(store) == []
     s = DataSource(metadata={'original_name': 'blah'})
     store.add(s._tok, s)
+    time.sleep(0.2)
+
     store.ttl = 0
     assert list(store) == [s._tok]
     assert store.get_tok(s) == s._tok
     assert store.needs_refresh(s) is False  # because it has no TTL
+
     store.remove(s)
+    time.sleep(0.2)
+
     assert list(store) == []
     assert os.path.exists(store.pdir)
     store.clear()
+    time.sleep(0.2)
+
     assert not os.path.exists(store.pdir)
     assert list(store) == []
