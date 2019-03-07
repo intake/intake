2113d445c18a374e6f46966f648a76481be3302e
Julia Signell <jsignell@gmail.com>
2019-03-07 16:46:30 -0500

    Stubbing out tests

---
+++ intake/gui/tests/test_source_view.py
@@ -0,0 +1,5 @@
+intake/gui/tests/test_source_view.py::test_description PASSED                                       [ 82%]
+intake/gui/tests/test_source_view.py::test_description_from_sources PASSED                          [ 86%]
+intake/gui/tests/test_source_view.py::test_description_clears_if_visible_is_set_to_false PASSED     [ 91%]
+intake/gui/tests/test_source_view.py::test_description_with_fake_driver_prints_error PASSED         [ 95%]
+intake/gui/tests/test_source_view.py::test_description_set_source_updates_panel_objects PASSED
