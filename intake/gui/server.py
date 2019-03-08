08260c941cdd4f95d28bb19a8dd442ab0d0ac7c9
Julia Signell <jsignell@gmail.com>
2019-03-08 15:42:40 -0500

    Added a very simple panel server

---
+++ intake/gui/server.py
@@ -0,0 +1,16 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2019, Anaconda, Inc. and Intake contributors
+# All rights reserved.
+#
+# The full license is in the LICENSE file, distributed with this software.
+#-----------------------------------------------------------------------------
+"""
+The simplest possible panel server. To launch a panel server containing the intake gui
+run:
+
+panel serve intake/gui/server.py
+
+"""
+
+import intake
+intake.gui.panel.servable()
