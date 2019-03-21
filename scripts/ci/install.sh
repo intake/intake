bcdd2071b711acfab6a20691850c1be8de847d5d
Julia Signell <jsignell@gmail.com>
2019-03-21 13:12:22 -0400

    Getting rid of git install via pip and trying to test better

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -25,6 +25,9 @@ conda install --yes ${CONDA_REQS}
 
 echo "Installing test dependencies."
 conda install --yes `python scripts/deps.py` -c defaults -c conda-forge
-conda install -y -c pyviz/label/dev -c bokeh/label/dev panel bokeh hvplot  # this is temporary
 conda install -y -c conda-forge nodejs
 npm install -g dat
+
+echo "Installing special versions of bokeh and panel - this is temporary"
+conda install -y -c pyviz/label/dev -c bokeh/label/dev panel bokeh hvplot holoviews
+git clone https://github.com/pyviz/panel.git && cd panel && python setup.py develop && cd ..
