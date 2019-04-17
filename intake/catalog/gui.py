3801ebeb8b580e0e27a012f51f51eea8dfa051ce
Julia Signell <jsignell@gmail.com>
2019-04-17 15:04:36 -0400

    Fixing up in catalog as well

--- intake/catalog/gui.py
+++ intake/catalog/gui.py
@@ -17,33 +17,33 @@ try:
             super().__init__(**kwargs)
 
         def setup(self):
-            self.plot = pn.widgets.Toggle(
+            self.plot_toggle = pn.widgets.Toggle(
                 name='📊',
                 value=False,
                 disabled=len(self.source.plots) == 0,
                 width=50)
 
             self.description = Description(source=self.source)
-            self.plotter = DefinedPlots(source=self.source,
-                                        visible=self.plot.value,
-                                        visible_callback=partial(setattr, self.plot, 'value'))
+            self.plot = DefinedPlots(source=self.source,
+                                     visible=self.plot_toggle.value,
+                                     visible_callback=partial(setattr, self.plot_toggle, 'value'))
 
             self.watchers = [
-                self.plot.link(self.plotter, value='visible'),
+                self.plot_toggle.link(self.plot, value='visible'),
             ]
 
             self.children = [
                 pn.Row(
                     pn.Column(
                         logo_file,
-                        self.plot
+                        self.plot_toggle
                     ),
                     self.description.panel,
                     background=BACKGROUND,
                     width_policy='max',
                     max_width=MAX_WIDTH,
                 ),
-                self.plotter.panel,
+                self.plot.panel,
             ]
 
         @property
@@ -57,7 +57,7 @@ try:
             super().__init__(**kwargs)
 
         def setup(self):
-            self.plot = pn.widgets.Toggle(
+            self.plot_toggle = pn.widgets.Toggle(
                 name='📊',
                 value=False,
                 width=50)
@@ -66,15 +66,15 @@ try:
                                                  enable_dependent=self.enable_plot)
             self.description = Description(source=self.sources)
 
-            self.plotter = DefinedPlots(source=self.sources,
-                                        visible=self.plot.value,
-                                        visible_callback=partial(setattr, self.plot, 'value'))
+            self.plot = DefinedPlots(source=self.sources,
+                                     visible=self.plot_toggle.value,
+                                     visible_callback=partial(setattr, self.plot_toggle, 'value'))
 
             # set disabled after item is selected
-            self.plot.disabled = self.item is None or len(self.item.plots) == 0
+            self.plot_toggle.disabled = self.item is None or len(self.item.plots) == 0
 
             self.watchers = [
-                self.plot.param.watch(self.on_click_plot, 'value'),
+                self.plot_toggle.param.watch(self.on_click_plot, 'value'),
                 self.source_browser.widget.link(self.description, value='source'),
             ]
 
@@ -82,7 +82,7 @@ try:
                 pn.Row(
                     pn.Column(
                         logo_file,
-                        self.plot
+                        self.plot_toggle
                     ),
                     self.source_browser.panel,
                     self.description.panel,
@@ -90,21 +90,21 @@ try:
                     width_policy='max',
                     max_width=MAX_WIDTH,
                 ),
-                self.plotter.panel,
+                self.plot.panel,
             ]
 
         def enable_plot(self, enable):
             if not enable:
-                self.plot.value = False
-                return enable_widget(self.plot, enable)
+                self.plot_toggle.value = False
+                return enable_widget(self.plot_toggle, enable)
 
         def on_click_plot(self, event):
             """ When the plot control is toggled, set visibility and hand down source"""
-            self.plotter.source = self.sources
-            self.plotter.visible = event.new
-            if self.plotter.visible:
-                self.plotter.watchers.append(
-                    self.source_browser.widget.link(self.plotter, value='source'))
+            self.plot.source = self.sources
+            self.plot.visible = event.new
+            if self.plot.visible:
+                self.plot.watchers.append(
+                    self.source_browser.widget.link(self.plot, value='source'))
 
         @property
         def sources(self):
