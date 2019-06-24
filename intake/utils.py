9cc09a386ceb71e2a79ffccd99111b1a45f0fc58
Sebastian Liem <s.liem@viriciti.com>
2019-06-24 16:47:58 +0200

    fix numpy warning, and future issue with elementwise comparison

--- intake/utils.py
+++ intake/utils.py
@@ -134,11 +134,12 @@ def pretty_describe(object, nestedness=0, indent=2):
     return out
 
 def decode_datetime(obj):
-    if b"__datetime__" in obj:
+    import numpy
+    if not isinstance(obj, numpy.ndarray) and "__datetime__" in obj:
         obj = datetime.datetime.strptime(obj["as_str"], "%Y%m%dT%H:%M:%S.%f%z")
     return obj
 
 def encode_datetime(obj):
     if isinstance(obj, datetime.datetime):
-        return {'__datetime__': True, 'as_str': obj.strftime("%Y%m%dT%H:%M:%S.%f%z")}
+        return {"__datetime__": True, "as_str": obj.strftime("%Y%m%dT%H:%M:%S.%f%z")}
     return obj
