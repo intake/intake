a12a3778c216fa69edddee03b6d6d50ac4f490f7
Sebastian Liem <s.liem@viriciti.com>
2019-06-24 17:21:22 +0200

    handle lack of tz info

--- intake/utils.py
+++ intake/utils.py
@@ -136,7 +136,16 @@ def pretty_describe(object, nestedness=0, indent=2):
 def decode_datetime(obj):
     import numpy
     if not isinstance(obj, numpy.ndarray) and "__datetime__" in obj:
-        obj = datetime.datetime.strptime(obj["as_str"], "%Y%m%dT%H:%M:%S.%f%z")
+        try:
+            obj = datetime.datetime.strptime(
+                    obj["as_str"],
+                    "%Y%m%dT%H:%M:%S.%f%z",
+            )
+        except ValueError: # Perhaps lacking tz info
+            obj = datetime.datetime.strptime(
+                    obj["as_str"],
+                    "%Y%m%dT%H:%M:%S.%f",
+            )
     return obj
 
 def encode_datetime(obj):
