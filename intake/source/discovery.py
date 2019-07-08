0ac39aff27f7978209675036754dceebec319157
Dan Allan <dallan@bnl.gov>
2019-07-08 17:46:54 -0400

    Incorporate 'drivers' section in config.

--- intake/source/discovery.py
+++ intake/source/discovery.py
@@ -19,20 +19,91 @@ import yaml
 
 from .base import DataSource
 from ..catalog.base import Catalog
-from ..config import confdir
+from ..config import load_conf, save_conf
 from ..utils import yaml_load
 logger = logging.getLogger('intake')
 
 
-def autodiscover(path=None, plugin_prefix='intake_'):
+def autodiscover(path=None, plugin_prefix='intake_', do_package_scan=True):
+    """Discover intake drivers.
+
+    In order of decreasing precedence:
+    - Respect the 'drivers' section of the intake configuration file.
+    - Find 'intake.drivers' entrypoints provided by any Python packages in the
+      environment.
+    - Search all packages in the environment for names that begin with
+      ``intake_``. Import them and scan them for subclasses of
+      ``intake.source.base.Plugin``. This was previously the *only* mechanism
+      for auto-discoverying intake drivers, and it is maintained for backward
+      compatibility. In a future release, intake will issue a warning if any
+      packages are located by the method that do not also have entrypoints.
+
+    Parameters
+    ----------
+    path : str or None
+        Default is ``sys.path``.
+    plugin_prefix : str
+        DEPRECATED. Default is 'intake_'.
+    do_package_scan : boolean
+        Default is True.
+
+    Returns
+    -------
+    drivers : dict
+        Name mapped to driver class.
+    """
+    if do_package_scan:
+        package_scan_results = _package_scan(path, plugin_prefix)
+
+    drivers = {}
+    group = entrypoints.get_group_all('intake.drivers', path=path)
+    for entrypoint in group:
+        # TODO Refer to intake configuration file to enable only a *subset* of
+        # the installed extensions and/or to give precedence to a specific
+        # driver in the event of a name collision.
+        try:
+            drivers[entrypoint.name] = entrypoint.load()
+        except ModuleNotFoundError:
+            log.error(
+                f"Failed to load {entrypoint.name} driver because module "
+                f"{entrypoint.module_name} could not be found.")
+            continue
+        except ModuleNotFoundError:
+            log.error(
+                f"Failed to load {entrypoint.name} driver because no object "
+                f"named {entrypoint.object_name} could be found in the module "
+                f"{entrypoint.module_name}.")
+            continue
+
+    # TODO Un-comment this code to unleash warnings after most known intake-*
+    # packages have been updated to provide 'intake.drivers' entrypoints.
+    # missing_entry_points = set(package_scan_results) - set(drivers)
+    # if missing_entry_points:
+    #     warnings.warn(
+    #         f"The drivers {missing_entry_points} do not specify entry_points "
+    #         f"and were only discovered via a package scan. This may break in a "
+    #         f"future release of intake. The packages should be updated.")
+    drivers.update(package_scan_results)
+
+    drivers_conf = (load_conf() or {}).get('drivers', {})
+    for name, dotted_object_name in drivers_conf.items():
+        if not dotted_object_name:
+            # This driver is disabled.
+            drivers.pop(name, None)
+        module_name, object_name = dotted_object_name.rpartition('.')
+        mock_entrypoint = entrypoint.EntryPoint(name, module_name, object_name)
+        # If the user has a broken config and this fails, let this raise
+        # instead of just logging.
+        drivers[entrypoint.name] = entrypoint.load()
+    return drivers
+
+def _package_scan(path=None, plugin_prefix='intake_'):
     """Scan for intake drivers and return a dict of plugins.
 
-    This wraps ``entrypoints.get_group_named('intake.drivers')``. In addition,
-    for backward-compatibility, this also searches path (or sys.path) for
-    packages with names that start with plugin_prefix.  Those modules will be
-    imported and scanned for subclasses of intake.source.base.Plugin.  Any
-    subclasses found will be instantiated and returned in a dictionary, with
-    the plugin's name attribute as the key.
+    This searches path (or sys.path) for packages with names that start with
+    plugin_prefix.  Those modules will be imported and scanned for subclasses
+    of intake.source.base.Plugin.  Any subclasses found will be instantiated
+    and returned in a dictionary, with the plugin's name attribute as the key.
     """
 
     plugins = {}
@@ -55,13 +126,6 @@ def autodiscover(path=None, plugin_prefix='intake_'):
                 else:
                     plugins[plugin_name] = plugin
             logger.debug("Import %s took: %7.2f s" % (name, time.time() - t))
-
-    group = entrypoints.get_group_all('intake.drivers')
-    for entrypoint in group:
-        # TODO Refer to intake configuration file to enable only a *subset* of
-        # the installed extensions and/or to give precedence to a specific
-        # driver in the event of a name collision.
-        plugins[entrypoint.name] = entrypoint.load()
     return plugins
 
 
