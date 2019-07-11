91666f466da14ee3aec6562147874f8b62cfd781
Martin Durant <martin.durant@utoronto.ca>
2019-07-11 14:27:38 -0400

    simple build

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -2,17 +2,4 @@
 
 set -e # exit on error
 
-echo "Building conda package."
-conda build -c defaults -c conda-forge --no-test ./conda
-
-# If tagged, upload package to main channel, otherwise, run tests
-if [ -n "$TRAVIS_TAG" ]; then
-    echo "Uploading conda package."
-    anaconda -t ${ANACONDA_TOKEN} upload -u intake --force `conda build --output ./conda`
-else
-    echo "Installing conda package locally."
-    conda install -y --use-local intake
-
-    echo "Running unit tests."
-    py.test
-fi
+pytest -v
