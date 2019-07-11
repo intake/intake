06167756a9fab52457df6e25e846a519dca9a1e3
Martin Durant <martin.durant@utoronto.ca>
2019-07-11 15:04:43 -0400

    wget syntax

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e # exit on error
-wget -L -O http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+wget "https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh" -O miniconda.sh
 bash miniconda.sh -b
 
 echo "Configuring conda."
