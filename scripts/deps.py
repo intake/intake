1ae47ea2571a5281ca992117abfd732b7f7fe6c2
Martin Durant <martin.durant@utoronto.ca>
2019-07-11 14:08:50 -0400

    simplify travis installs

--- scripts/deps.py
+++ scripts/deps.py
@@ -9,17 +9,14 @@
 import os
 import sys
 
-from conda_build.api import render
-
 # This is needed because render spams the console
 old_stdout = sys.stdout
 sys.stdout = open(os.devnull, 'w')
 sys.stderr = open(os.devnull, 'w')
 
-meta = render('conda')[0][0].meta
-
 sys.stdout = old_stdout
 
 test_requires = [l.strip() for l in open('test_requirements.txt')]
+requires = [l.strip() for l in open('requirements.txt') if not l.startswith("#")]
 
-print(' '.join(meta['test']['requires'] + test_requires))
+print(' '.join(requires + test_requires))
