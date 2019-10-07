527fbaedc525aaa3b933b3ef0717a98283baf39a
Garrett Bischof <gbischof@bnl.gov>
2019-10-07 14:49:39 -0400

    Update to test

--- intake/catalog/tests/test_discovery.py
+++ intake/catalog/tests/test_discovery.py
@@ -1,15 +1,15 @@
-from ..discovery import MergedCatalog, EntrypointsCatalog
-from ..local import YAMLFilesCatalog
 import copy
-import os
 import glob
+import os
 import pytest
+
+from ..local import YAMLFilesCatalog, MergedCatalog, EntrypointsCatalog
+
 
 def test_catalog_discovery():
     basedir = os.path.dirname(__file__)
     path = os.path.join(basedir, 'catalog_search')
     collision_path = os.path.join(path, 'yaml')
-
 
     test_catalog = MergedCatalog([EntrypointsCatalog(paths=[path]),
                                   YAMLFilesCatalog(path=[path])])
