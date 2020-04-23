9994435595a7c67f1bd766b96570d519609d9b87
Martin Durant <martin.durant@utoronto.ca>
2020-04-23 13:32:34 -0400

    Remove CI package pins

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     python_requires=">=3.6",
     long_description=open('README.md').read(),
