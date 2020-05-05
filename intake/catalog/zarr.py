80c73c4e9aba8dbee31aaf708bb17d5d74c519ac
Alistair Miles <alimanfoo@googlemail.com>
2020-05-05 10:07:48 +0100

    further development of zarr catalog

--- intake/catalog/zarr.py
+++ intake/catalog/zarr.py
@@ -3,36 +3,78 @@ from .local import LocalCatalogEntry
 from ..source import register_driver
 
 
-class ZarrCatalog(Catalog):
-    """Catalog as described by a Zarr group."""
+class ZarrGroupCatalog(Catalog):
+    """A catalog of the members of a Zarr group."""
+
     version = '0.0.1'
     container = 'catalog'
     partition_access = None
     name = 'zarr_cat'
 
-    def __init__(self, url, consolidated=False, **kwargs):
+    def __init__(self, url, storage_options=None, component=None, metadata=None,
+                 consolidated=False):
+        """
+
+        Parameters
+        ----------
+        url : str
+            Location of data file(s), possibly including protocol information
+        storage_options : dict, optional
+            Passed on to storage backend for remote files
+        component : str, optional
+            If None, build a catalog from the root group. If given, build the
+            catalog from the group at this location in the hierarchy.
+        metadata : dict, optional
+            Catalog metadata. If not provided, will be populated from Zarr
+            group attributes.
+        consolidated : bool, optional
+            If True, assume Zarr metadata has been consolidated.
+        """
         self._url = url
+        self._storage_options = storage_options or {}
+        self._component = component
         self._consolidated = consolidated
         self._grp = None
-        super().__init__(**kwargs)
+        super().__init__(metadata=metadata)
 
     def _load(self):
         import zarr
 
         if self._grp is None:
-            # obtain the zarr group to wrap as a catalog
+
+            # obtain the zarr root group
             if isinstance(self._url, zarr.hierarchy.Group):
-                # use already-opened group
+                # use already-opened group, allows support for nested groups
+                # as catalogs
                 self._grp = self._url
+
             else:
-                # open root group from url
-                from fsspec import get_mapper
-                store = get_mapper(self._url)
+
+                # obtain store
+                if isinstance(self._url, str):
+                    # open store from url
+                    from fsspec import get_mapper
+                    store = get_mapper(self._url, **self._storage_options)
+                else:
+                    # assume store passed directly
+                    store = self._url
+
+                # open root group
                 if self._consolidated:
                     # use consolidated metadata
-                    self._grp = zarr.open_consolidated(store=store, mode='r')
+                    root = zarr.open_consolidated(store=store, mode='r')
                 else:
-                    self._grp = zarr.open_group(store=store, mode='r')
+                    root = zarr.open_group(store=store, mode='r')
+
+                # deal with component path
+                if self._component is None:
+                    self._grp = root
+                else:
+                    self._grp = root[self._component]
+
+            # use zarr attributes as metadata
+            if not self.metadata:
+                self.metadata = self._grp.attrs.asdict()
 
         # build catalog entries
         entries = {}
@@ -53,6 +95,3 @@ class ZarrCatalog(Catalog):
 
     def to_zarr(self):
         return self._grp
-
-
-register_driver('zarr_cat', ZarrCatalog)
