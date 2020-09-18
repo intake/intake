77f5182958dc75bb5807cf122b26062271e518da
Martin Durant <martindurant@users.noreply.github.com>
2020-09-18 10:22:17 -0400

    long line

--- intake/source/utils.py
+++ intake/source/utils.py
@@ -251,7 +251,8 @@ def path_to_glob(path):
     prev_field_name = None
     for literal_text, field_name, format_spec, conversion in fmt.parse(path):
         glob += literal_text
-        if field_name and not('' == literal_text and prev_field_name):  # condition to avoid repeated * on adjacent fields
+        # condition to avoid repeated * on adjacent fields
+        if field_name and not('' == literal_text and prev_field_name):
             glob += '*'
         prev_literal = literal_text
         prev_field_name = field_name
