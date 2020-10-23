e698cd8512a9d39948b55e33fee4e70440abf5b3
Dan Allan <dallan@bnl.gov>
2020-10-23 17:15:37 -0400

    Add test of deferred population of intake.cat.

--- intake/catalog/tests/test_discovery.py
+++ intake/catalog/tests/test_discovery.py
@@ -2,6 +2,7 @@ import copy
 import glob
 import os
 import pytest
+import sys
 
 from ..local import YAMLFilesCatalog, MergedCatalog, EntrypointsCatalog
 
@@ -16,3 +17,19 @@ def test_catalog_discovery():
 
     assert 'use_example1' in test_catalog
     assert 'ep1' in test_catalog
+
+
+def test_deferred_import():
+    "See https://github.com/intake/intake/pull/541"
+    # We are going to mess with sys.modules here, so to be safe let's put it
+    # back the way it was at the end.
+    modules = sys.modules.copy()
+    del sys.modules["intake"]
+    try:
+        import intake
+        assert intake.catalog is None
+        assert intake.cat is not None
+        assert intake.catalog is not None  # implementation detail
+    finally:
+        # Put sys.modules back as it was before we messed with it.
+        sys.modules = modules
