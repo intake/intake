b2b21ec3997823d43cb2181f042db224e292da62
Martin Durant <martin.durant@utoronto.ca>
2021-11-01 12:52:15 -0400

    fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -182,6 +182,3 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 numpydoc_show_class_members = False
 numpydoc_show_inherited_class_members = False
 numpydoc_class_members_toctree = False
-
-def setup(app):
-    app.add_stylesheet('css/custom.css')
