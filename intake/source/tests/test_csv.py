23eafe4401c8f53eedf32c80c8de07b29ec3b09d
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2023-09-06 10:33:15 +0200

    Changed how skipfooter CSV files are stored and accessed for testing.
    This change was necessary to prevent conflicts with tests that rely on the
    content of source.tests.test_csv.data_filenames conforming to the original
    content, or that don't tolerate extra CSV files being added to
    intake/source/tests.
    
    After the change, all tests in `intake/source/tests/test_csv.py` pass, but many
    other tests fail. Need to rerun tests in a different branch to confirm whether
    this is caused by the new code, or whether it is a problem in the original
    master branch.

--- intake/source/tests/test_csv.py
+++ intake/source/tests/test_csv.py
@@ -26,10 +26,7 @@ def data_filenames():
         sample2_1=posixpath.join(basedir, "sample2_1.csv"),
         sample2_2=posixpath.join(basedir, "sample2_2.csv"),
         sample2_all=posixpath.join(basedir, "sample2_*.csv"),
-        sample_pattern=posixpath.join(basedir, "sample{num:d}_{dup:d}.csv"),
-        sample_fewfooters=posixpath.join(basedir, "sample_fewfooters.csv"),
-        sample_manyfooters=posixpath.join(basedir, "sample_manyfooters.csv"),
-        sample_nofooters=posixpath.join(basedir, "sample_nofooters.csv")
+        sample_pattern=posixpath.join(basedir, "sample{num:d}_{dup:d}.csv")
     )
 
 
@@ -69,11 +66,16 @@ def sample_pattern_datasource_with_cache(data_filenames):
     return csv.CSVSource(data_filenames["sample_pattern"], metadata=metadata)
 
 
-@pytest.fixture(params=[('sample_fewfooters', 2), ('sample_manyfooters', 5)])
-def sample_datasource_with_skipfooter(request, data_filenames):
-    filekey, skipfooter = request.param
+@pytest.fixture
+def footer_csv_dir():
+    return make_path_posix(os.path.join(os.path.dirname(__file__), "footer_csvs"))
+
+@pytest.fixture(params=[('sample_fewfooters.csv', 2), ('sample_manyfooters.csv', 5)])
+def sample_datasource_with_skipfooter(request, footer_csv_dir):
+    csv_filename, skipfooter = request.param
+    csv_path=posixpath.join(footer_csv_dir, csv_filename)
     return csv.CSVSource(
-        data_filenames[filekey],
+        csv_path,
         csv_kwargs={"skipfooter": skipfooter}
     )
 
@@ -282,8 +284,9 @@ def test_pickle(sample1_datasource):
     assert expected_df.equals(df)
 
 
-def test_skipfooter(sample_datasource_with_skipfooter, data_filenames):
-    expected_source = csv.CSVSource(data_filenames["sample_nofooters"])
+def test_skipfooter(sample_datasource_with_skipfooter, footer_csv_dir):
+    csv_nofooters=posixpath.join(footer_csv_dir, "sample_nofooters.csv")
+    expected_source = csv.CSVSource(csv_nofooters)
     expected_schema = expected_source.discover()
     assert expected_schema == sample_datasource_with_skipfooter.discover()
     expected_df = expected_source.read()
