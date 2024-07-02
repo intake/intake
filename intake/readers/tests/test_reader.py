b1b265d7bfb45a2d4252b6c1df378b74d7a317f9
Martin Durant <martin.durant@alumni.utoronto.ca>
2024-07-02 15:36:02 -0400

    Readd pattern globs for xarray

--- intake/readers/tests/test_reader.py
+++ intake/readers/tests/test_reader.py
@@ -1,5 +1,7 @@
 import tempfile
 
+import pytest
+
 import intake
 
 
@@ -20,3 +22,47 @@ def test_reader_from_call():
         read_df = reader.read()
         assert all(read_df.col1 == df.col1)
         assert all(read_df.col2 == df.col2)
+
+
+@pytest.fixture()
+def xarray_dataset():
+    xr = pytest.importorskip("xarray")
+    import numpy as np
+    import pandas as pd
+
+    temperature = 15 + 8 * np.random.randn(2, 3, 4)
+    lon = [-99.83, -99.32]
+    lat = [42.25, 42.21]
+    instruments = ["manufac1", "manufac2", "manufac3"]
+    time = pd.date_range("2014-09-06", periods=4)
+    return xr.Dataset(
+        data_vars=dict(
+            temperature=(["loc", "instrument", "time"], temperature),
+        ),
+        coords=dict(
+            lon=("loc", lon),
+            lat=("loc", lat),
+            instrument=instruments,
+            time=time,
+        ),
+    )
+
+
+def test_xarray_pattern(tmpdir, xarray_dataset):
+    path1 = f"{tmpdir}/1.nc"
+    path2 = f"{tmpdir}/2.nc"
+    xarray_dataset.to_netcdf(path1)
+    xarray_dataset.to_netcdf(path2)
+
+    data = intake.datatypes.HDF5("%s/{part}.nc" % tmpdir)
+    reader = intake.readers.XarrayPatternReader(data)
+    ds = reader.read()
+
+    assert ds.part.values.tolist() == ["1", "2"]
+    assert ds.temperature.shape == (2, 2, 3, 4)
+
+    data = intake.datatypes.HDF5("%s/{part:d}.nc" % tmpdir)
+    reader = intake.readers.XarrayPatternReader(data)
+    ds = reader.read()
+
+    assert ds.part.values.tolist() == [1, 2]
